fb01cc2e2988808119f2c43d3de8863c187614ee
Rohan Sampath <rohan.sampath@gmail.com>
2023-02-05 10:11:51 -0800

    Add project files.

---
+++ LRUCache/LRUCache.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRUCache
+{
+
+    public interface ICache<TKey, TValue>
+    {
+        bool TryGetValue(TKey key, out TValue value);
+        void Put(TKey key, TValue value);
+
+    }
+
+    public class LRUCache<TKey, TValue> : ICache<TKey, TValue>
+    {
+        LinkedList<KeyValuePair<TKey, TValue>> list;
+        Dictionary<TKey, LinkedListNode<KeyValuePair<TKey, TValue>>> dict;
+
+
+        public int Count => dict.Count;
+        int limit; 
+
+        public LRUCache(int limit)
+        {
+           dict = new Dictionary<TKey, LinkedListNode<KeyValuePair<TKey, TValue>>>(limit);
+           list = new LinkedList<KeyValuePair<TKey, TValue>>();
+            this.limit = limit; 
+        }
+
+        public void Put(TKey key, TValue value)
+        {
+            if(dict.ContainsKey(key))
+            {
+                var temp = dict[key];
+
+                list.Remove(temp);
+
+                var pair = new KeyValuePair<TKey, TValue>(key, value);
+                list.AddFirst(pair); 
+                dict[key] = list.First; 
+            }
+            else
+            {
+                var temp = new LinkedListNode<KeyValuePair<TKey, TValue>>(new KeyValuePair<TKey, TValue>(key, value));
+                list.AddFirst(temp);
+                dict.Add(key, temp);
+                if (list.Count > limit)
+                {
+                    var temp2 = list.Last; 
+                    list.Remove(temp2);
+                    dict.Remove(temp2.Value.Key); 
+                }
+            }
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            if(dict.ContainsKey(key))
+            {
+                var temp = dict[key];
+
+                list.Remove(temp);
+                list.AddFirst(temp);
+
+                value = temp.Value.Value; 
+
+                return true;
+
+            }
+            value = default; 
+            return false;
+        }
+    }
+}
---
+++ LRUCache/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LRUCache
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LRUCache<string, string> cache = new LRUCache<string, string>(5); 
+
+            cache.Put("a", "b");    
+            cache.Put("d", "c");
+            cache.Put("e", "f");
+            cache.Put("g", "h");
+            cache.Put("q", "p");
+
+            var temp = cache.TryGetValue("a", out string temp2);
+            ;
+            cache.Put("a", "c"); 
+            temp = cache.TryGetValue("x", out string temp3);
+            temp = cache.TryGetValue("c", out string temp4);
+            cache.Put("d", "c");
+            cache.Put("v", "n");
+            cache.Put("t", "t");
+            cache.Put("t", "r");
+            cache.Put("w", "a");
+            cache.Put("q", "o");
+            cache.TryGetValue("g", out string temp1);
+            ;
+
+        }
+    }
+}
